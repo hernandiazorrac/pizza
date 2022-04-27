e884d4f9b683956f5874c2cfa4af6e83ad599784
Hernán Díaz Orrac <hernandiazorrac@gmail.com>
2022-04-27 13:32:37 -0300

    adición de productos al carrito en consola

--- js/main.js
+++ js/main.js
@@ -3,40 +3,30 @@ let pizzaArrayR = ['champ2.png', 'pepperoni2.png', 'napolitana2.png', 'margherit
 
 // ------------------------------------------------------------ //
 
-const pizzasVariedad = [{nombre: "Pepperoni", precio: 700, img: "../assets/img/pepperoni200.png"}, {nombre: "Margarita", precio: 700, img: "../assets/img/margherita200.png"}, {nombre: "Napolitana", descripcion: "Salsa, parmesano, albahaca.", precio: 900, img: "../assets/img/napolitana200.png"}, {nombre: "Champignones", precio: 700, img: "../assets/img/champ200.png"}, {nombre: "Cebolla", precio: 800, img: "../assets/img/onion200.png"}, {nombre: "Salchicha", precio: 700, img: "../assets/img/sausage200.png"}, ]
+const pizzasVariedad = [{nombre: "Pepperoni", precio: 700, img: "../assets/img/pepperoni200.png"}, {nombre: "Margarita", precio: 700, img: "../assets/img/margherita200.png"}, {nombre: "Napolitana", descripcion: "Salsa, parmesano, albahaca.", precio: 900, img: "../assets/img/napolitana200.png"}, {nombre: "Champignones", precio: 700, img: "../assets/img/champ200.png"}, {nombre: "Cebolla", precio: 800, img: "../assets/img/onion200.png"}, {nombre: "Salchichas", precio: 700, img: "../assets/img/sausage200.png"}, ]
 
 // ------------------------------------------------------------ //
 let cart = []
-
 // ------------------------------------------------------------ //
-
-//alert("\n¿No te decidís qué pizza pedir? \nNosotros nos encargamos de eso.")
-
-// for(let pizzas of pizzasVariedad){
-//     pizzaCard.innerHTML += `<div class="card text-center p-3" style="width: 18rem;">
-//     <img src="${pizzas.img}" class="card-img-top" alt="...">
-//     <div class="card-body">
-//       <h5 class="card-title">${pizzas.nombre}</h5>
-//       <p class="card-text">$${pizzas.precio}</p>
-//       <button type="button" class="btn btn-warning" onclick="addToCart()">Agregar al carrito</button>
-//     </div>
-//     </div>`
-//     }
 
 randomImg();
 
-function randomImg(){ //genera imagen aleatoria
-    let randomArray = Math.floor(Math.random() * pizzaArrayL.length); //elige un valor aleatorio del array
+//genera imagen aleatoria
+function randomImg(){ 
+    //elige un valor aleatorio del array
+    let randomArray = Math.floor(Math.random() * pizzaArrayL.length);
     let randomArray2 = Math.floor(Math.random() * pizzaArrayR.length);
 
     let selectImg = pizzaArrayL[randomArray]
     let selectImg2 = pizzaArrayR[randomArray2]
 
-    document.getElementById('imgPizza').src = `./assets/img/${selectImg}`; //dirección de donde se obtienen las imágenes
+    //dirección de donde se obtienen las imágenes
+    document.getElementById('imgPizza').src = `./assets/img/${selectImg}`; 
     document.getElementById('imgPizza2').src = `./assets/img/${selectImg2}`;
 }
 
-function confirmPizza(){ //pop up de confirmar pedido
+//pop up de confirmar pedido
+function confirmPizza(){
     let confirmar = confirm("\n¿Querés confirmar tu pedido?")
         if(confirmar){
             console.log("¡Muchas gracias por tu compra!")
@@ -45,8 +35,8 @@ function confirmPizza(){ //pop up de confirmar pedido
         }
 }
 
+//agregar al carrito
 function addToCart(item){
     cart.push(item);
-    console.log(cart);
+    console.table(cart);
 }
-
