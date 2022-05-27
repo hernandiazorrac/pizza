2860cfb811ab2d129c8853514a94082d2e7fe687
Hernán Díaz Orrac <hernandiazorrac@gmail.com>
2022-05-27 19:28:37 -0300

    json fetch

--- js/main.js
+++ js/main.js
@@ -148,7 +148,7 @@ window.addEventListener('DOMContentLoaded', () => {
 })
 
 function loadJSON(){
-    fetch('https://github.com/hernandiazorrac/pizza/blob/main/json/pizzas.json') // hace fetch al json hosteado en github
+    fetch('https://hernandiazorrac.github.io/json/pizzas.json') // hace fetch al json hosteado en github
     //fetch('./json/pizzas.json')  -> descomentar para usar live server
     .then(response => response.json())
     .then(data => {
